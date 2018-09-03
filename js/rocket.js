3e6a5809d76113fd6fb6c2ed5c2389f7013798a1
modality consulting <ideas@modalityconsulting.com>
2018-09-03 13:44:49 -0400

    made it harder

--- js/rocket.js
+++ js/rocket.js
@@ -35,7 +35,7 @@ var changeState = function (state) {
                 var randomNumber = Math.round(Math.random()*10);
                 console.log('randomNumber:',randomNumber)
                 // success
-                if (randomNumber > 2) {
+                if (randomNumber > 6) {
                       changeState(4);
 
                 } else {
